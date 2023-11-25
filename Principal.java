3bea7770f612634c3bd70db9915554b40dbad3b2
linsignares <142268649+linsignares@users.noreply.github.com>
2023-11-24 22:03:26 -0500

    Create Principal.java

---
+++ Principal.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.poo_t6;
+
+/**
+ *
+ * @author julianainsignares
+ */
+public class POO_T6 {
+
+    public static void main(String[] args) {
+        interfaz ventana = new interfaz();
+        ventana.setVisible(true);
+    }
+}
